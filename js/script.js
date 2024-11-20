1b640f81846017b3405022fe0cc39aa168b967d9
ydr29913 <ydr29913@educastur.es>
2024-11-20 12:34:58 +0100

    Modificado script.js en rama Main

--- js/script.js
+++ js/script.js
@@ -21,5 +21,10 @@ document.addEventListener("DOMContentLoaded", function() {
         if (!valid) {
             event.preventDefault();
         }
+
+        if (mensaje.value.trim() === "") {
+            alert("Ingrese un mensaje, por favor");
+            valid = false;
+        }
     });
 });
